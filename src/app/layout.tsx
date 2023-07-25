f142b1fb1963b447b6b87dff478058921579d1bf
Daniel <daniel@stackfive.io>
2023-07-25 10:16:31 -0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Daniel Kolesnitski",
+  description:
+    "Daniel Kolesnitski is a full-stack developer who builds cutting edge and innovative apps for the web",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
